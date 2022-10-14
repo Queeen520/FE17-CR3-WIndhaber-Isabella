342a6e8b5e67a3cc117702a72d6926ebe0a4bceb
Isabella W <isabellawindhaber@gmail.com>
2022-10-14 17:24:28 +0200

    -

--- DeliveryPage/src/app/menu/menu.component.ts
+++ DeliveryPage/src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { menus } from '../menus';
+import { interfaceMenus } from '../interfaceMenus';
 
 @Component({
   selector: 'app-menu',
@@ -7,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  menus: Array<interfaceMenus> = menus;
   constructor() { }
 
   ngOnInit(): void {
