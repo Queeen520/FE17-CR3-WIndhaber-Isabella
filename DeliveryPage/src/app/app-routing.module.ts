391979f5154e412909a080dd1d8a004154022fa9
Isabella W <isabellawindhaber@gmail.com>
2022-10-14 20:15:55 +0200

    --

--- DeliveryPage/src/app/app-routing.module.ts
+++ DeliveryPage/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component'
 
 const routes: Routes = [
   {
     path: "", component: HomeComponent
+  },
+  {
+    path: "home", component: HomeComponent
+  },
+  {
+    path: "menu", component: MenuComponent
   }
 ];
 
